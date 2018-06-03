a17a6e97abbdc6946ceeaf82f85711246474c505
Alex Wilson <fonzmp@gmail.com>
2018-06-03 23:48:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,6 +51,7 @@ class InteractiveRecord
 
   def self.find_by(option)
     sql = "SELECT * FROM #{self.table_name} WHERE #{col_names_for_insert} = ?"
+    DB[:conn].execute(sql, option)
   end
 
 end
