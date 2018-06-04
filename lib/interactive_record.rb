7465bd63d9d566117e0981490b8d77b54edf761e
Alex Wilson <fonzmp@gmail.com>
2018-06-04 00:00:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
       @option_value = value
     end
     sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
-    DB[:conn].execute(sql, option_key, option_value)
+    DB[:conn].execute(sql, @option_key, @option_value)
   end
 
 end
